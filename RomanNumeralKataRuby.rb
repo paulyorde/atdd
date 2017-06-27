54c6fba1fec41db111680b1aea8ff2fed99eff0b
paulyorde <paulyorde26@gmail.com>
2017-06-27 09:46:56 -0400

    refactored appendNumeral while vs. if

--- RomanNumeralKataRuby.rb
+++ RomanNumeralKataRuby.rb
@@ -1,25 +1,25 @@
 require 'rspec'
 
 describe 'roman numeral converter' do
-	# it 'should convert 1000 to M' do
-	# 	expect(convert_to_numeral 1000).to eql "M"
-	# end
+	it 'should convert 1000 to M' do
+		expect(convert_to_numeral 1000).to eql "M"
+	end
 
-	# it 'should convert 500 to D' do
-	# 	expect(convert_to_numeral 500).to eql "D"
-	# end
+	it 'should convert 500 to D' do
+		expect(convert_to_numeral 500).to eql "D"
+	end
 
-	# it 'should convert 100 to C' do
-	# 	expect(convert_to_numeral 100).to eql "C"
-	# end
+	it 'should convert 100 to C' do
+		expect(convert_to_numeral 100).to eql "C"
+	end
 
-	# it 'should convert 50 to L' do
-	# 	expect(convert_to_numeral 50).to eql "L"
-	# end
+	it 'should convert 50 to L' do
+		expect(convert_to_numeral 50).to eql "L"
+	end
 
-	# it 'should convert 10 to X' do
-	# 	expect(convert_to_numeral 10).to eql "X"
-	# end
+	it 'should convert 10 to X' do
+		expect(convert_to_numeral 10).to eql "X"
+	end
 
 	it 'should convert 9 to IX' do
 		expect(convert_to_numeral 9).to eql "IX"
@@ -59,48 +59,23 @@ describe 'roman numeral converter' do
 
 end
 
-
-
 def convert_to_numeral number
-	values = [9,5,4]
-	symbols = ["IX","V","IV"]
+	values = [1000,500,100,50,10,9,5,4,1]
+	symbols = ["M","D","C","L","X","IX","V","IV","I"]
 
 	remaining = number
 	numeral = ""
 
 	values.each_index do |i|
-		remaining = appendNumeral remaining, values[i], symbols[i], numeral
+	 	remaining = appendNumeral remaining, values[i], symbols[i], numeral
 	end
-	
-	until remaining == 0
-		remaining -= 1
-		numeral << "I"
-	end
-	#until remaining == 0
-		# case
-		# 	when remaining >= 1000
-		# 		remaining -= 1000
-		# 		numeral << "M"
-		# 	when remaining >= 500 && remaining < 1000
-		# 		remaining -= 500
-		# 		numeral << "D"
-		# 	when remaining >= 100 && remaining < 500
-		# 		remaining -= 100
-		# 		numeral << "C"
-		# 	when remaining >= 50 && remaining < 100
-		# 		remaining -= 50
-		# 		numeral << "L"
-		# 	when remaining >= 10 && remaining < 50
-		# 		remaining -= 10
-		# 		numeral << "X"
-		# end
-	#end
+
 	numeral
 end
 
 def appendNumeral number, value, numeralToAppend, numeral
 	remaining = number
-	if number >= value	
+	while remaining >= value	
 		numeral << numeralToAppend
 		remaining -= value
 	end
