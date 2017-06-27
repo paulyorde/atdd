9658b752fe7d4d2cfd29715b97a46da3e20e394d
paulyorde <paulyorde26@gmail.com>
2017-06-27 07:09:18 -0400

    passed convert to D

--- RomanNumeralKataRuby.rb
+++ RomanNumeralKataRuby.rb
@@ -1,44 +1,31 @@
 require 'rspec'
 
-describe 'a thing' do
-	it 'should pass' do
-		expect(true).to be true
-	end
-end
-
 describe 'roman numeral converter' do
-	it 'should convert 1 to I' do
-		expect(convert_to_numeral 1).to eql "I"
+	it 'should convert 1000 to M' do
+		expect(convert_to_numeral 1000).to eql "M"
 	end
 
-	it 'should convert 4 to IV' do
-		expect(convert_to_numeral 4).to eql "IV"
+	it 'should convert 500 to D' do
+		expect(convert_to_numeral 500).to eql "D"
 	end
 
-	it 'should convert 5 to V' do
-		expect(convert_to_numeral 5).to eql "V"
-	end
-
-	it 'should convert 10 to X' do
-		expect(convert_to_numeral 10).to eql "X"
-	end
-
-	it 'should convert 11 to XI' do
-		expect(convert_to_numeral 11).to eql "XI"
-	end
 end
-
 
 def convert_to_numeral number
-	if number == 1
-		"I"
-	elsif number == 4
-		"IV"
-	elsif number == 5
-		"V"
-	elsif number == 10
-		"X"
+	remaining = number
+	numeral = ""
+	until remaining == 0
+		case
+			when remaining >= 1000
+				remaining -= 1000
+				numeral << "M"
+			when remaining >= 500 && remaining < 1000
+				remaining -= 500
+				numeral << "D"
+		end
+		return numeral 
 	end
+
 end
 
 
